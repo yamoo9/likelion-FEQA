65033957dd62a6d68818bde6df28a13284105bad
yamoo9 <yamoo9@naver.com>
2024-01-18 18:03:26 +0900

    [정리] 수업 종료 전

--- main.jsx
+++ main.jsx
@@ -2,10 +2,20 @@ import React from 'https://esm.sh/react';
 import { createRoot } from 'https://esm.sh/react-dom';
 
 
-createRoot(document.getElementById('root'))
-    .render(
-        <div id="app">
-            <h1>안녕!<br />리액트.</h1>
-            <p>리액트는 사용자 인터페이스 구축을 위한 JavaScript 오픈소스 라이브러리입니다.</p>
-        </div>
-    );
+const rootElement = document.getElementById('root');
+const partialElement = document.createElement('div');
+partialElement.setAttribute('id', 'partialRoot');
+document.body.append(partialElement);
+
+const reactDomRoot = createRoot(rootElement);
+const partialRoot = createRoot(partialElement);
+
+const app = (
+    <div id="app">
+        <h1>안녕!<br />리액트.</h1>
+        <p>리액트는 사용자 인터페이스 구축을 위한 JavaScript 오픈소스 라이브러리입니다.</p>
+    </div>
+);
+
+reactDomRoot.render(app);
+partialRoot.render(app);
