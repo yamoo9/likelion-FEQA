ed157b665e79878e936d425476a5b68c34961dae
yamoo9 <yamoo9@naver.com>
2024-01-18 15:51:26 +0900

    [실습] Vite + JSX

--- lecture/06-jsx-syntax.js
+++ lecture/06-jsx-syntax.js
@@ -8,8 +8,8 @@ import { createRoot } from 'https://esm.sh/react-dom';
 // (ex: Sass → CSS, TypeScript → JavaScript)
 ReactDOM.createRoot(document.getElementById('root'))
     .render(
-        <div id="app">
-            <h1>안녕!<br/>리액트.</h1>
-            <p>리액트는 사용자 인터페이스 구축을 위한 JavaScript 오픈소스 라이브러리입니다.</p>
-        </div>
+<div id="app">
+    <h1>안녕!<br/>리액트.</h1>
+    <p>리액트는 사용자 인터페이스 구축을 위한 JavaScript 오픈소스 라이브러리입니다.</p>
+</div>
     )
