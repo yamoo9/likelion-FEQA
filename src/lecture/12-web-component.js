3cfb1487674020b8acbbf57d2dd3345b1218f4a2
yamoo9 <yamoo9@naver.com>
2024-01-19 11:17:36 +0900

    [실습] 상태 변수를 설정해 모드 전환에 따른 리액트 리-렌더링

---
+++ src/lecture/12-web-component.js
@@ -0,0 +1,9 @@
+// 웹 컴포넌트 API (웹 표준)
+// globalThis.customElements.define('custom-element', CustomComponent)
+// <slot></slot>
+// <custom-element></custom-element>
+
+// <custom-element>
+//   <h2>headline</h2>
+//   <span>span element</span>
+// </custom-element>
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,20 @@
 import React from 'https://esm.sh/react';
 import { createRoot } from 'https://esm.sh/react-dom';
 
+const data = {
+    greetingMessage: ['안녕!', '리액트.'],
+    message: '리액트는 사용자 인터페이스 구축을 위한 JavaScript 오픈소스 라이브러리입니다.',
+};
 
-const createApp = () => (
+const anotherData = {
+    greetingMessage: ['행복한 아침이야', '오늘도 좋은 하루 되렴~'],
+    message: '나날이 성장하는 여러분을 보는 즐거움이 쏠쏠~ 😉'
+}
+
+const createApp = (data) => (
     <div id="app">
-        <h1>안녕!<br />리액트.</h1>
-        <p>리액트는 사용자 인터페이스 구축을 위한 JavaScript 오픈소스 라이브러리입니다.</p>
+        <h1>{data.greetingMessage[0]}<br />{data.greetingMessage[1]}</h1>
+        <p>{data.message}</p>
     </div>
 );
 
@@ -13,4 +22,27 @@ const rootElement = document.getElementById('root');
 const reactDomRoot = createRoot(rootElement);
 
 /* 함수 실행 => JSX -> React.createElement() -> 리액트 요소 : ReactElement */
-reactDomRoot.render(createApp());
+function render(mode = 'data') {
+    reactDomRoot.render(createApp(mode === 'data' ? data : anotherData));
+}
+
+render();
+
+
+// 버튼 이벤트 핸들링
+const button = document.querySelector('button');
+
+// 데이터 전환을 위한 상태 변수
+let mode = 'data'; // 'data' | 'anotherData'
+
+const handleChangeMessage = () => {
+    if (mode.includes('data')) {
+        mode = 'anotherData';
+    } else {
+        mode = 'data';
+    }
+
+    render(mode);
+};
+
+button.addEventListener('click', handleChangeMessage);
