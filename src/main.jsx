f5f0a9854539e141656a5321619e6ddc4b93b879
yamoo9 <yamoo9@naver.com>
2024-01-19 09:26:49 +0900

    [정리] 디렉토리
    - public
    - src

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'https://esm.sh/react';
 import { createRoot } from 'https://esm.sh/react-dom';
 
 
-const app = (
+const createApp = () => (
     <div id="app">
         <h1>안녕!<br />리액트.</h1>
         <p>리액트는 사용자 인터페이스 구축을 위한 JavaScript 오픈소스 라이브러리입니다.</p>
@@ -12,4 +12,5 @@ const app = (
 const rootElement = document.getElementById('root');
 const reactDomRoot = createRoot(rootElement);
 
-reactDomRoot.render(app);
+/* 함수 실행 => JSX -> React.createElement() -> 리액트 요소 : ReactElement */
+reactDomRoot.render(createApp());
