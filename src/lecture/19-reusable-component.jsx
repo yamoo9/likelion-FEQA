6668efdeab3a1532f957a3dd9b13aa13105a5a91
yamoo9 <yamoo9@naver.com>
2024-01-22 15:39:02 +0900

    [원론] 컴포넌트 재사용성

---
+++ src/lecture/19-reusable-component.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+// [학습 주제]
+// 컴포넌트 재사용
+
+// .formControl>label+input
+function FormControl() {
+  return (
+    <div className="formControl">
+      <label>
+        사용자 이름 <input type="text" name="username" />
+      </label>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div id="app">
+      <FormControl />
+      <FormControl />
+    </div>
+  );
+}
+
+createRoot(document.getElementById('root')).render(<App />);
