06bcf191c4adcdf946967c35cde9091fc8340154
yamoo9 <yamoo9@naver.com>
2024-01-23 15:51:26 +0900

    [실습] 버튼 컴포넌트

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import Widget from '../components/Widget/Widget';
+import Button from '../components/Button/Button';
 
 // const styles = {
 //   app: {
@@ -17,11 +18,23 @@ function App() {
         summary="어제보다 1.3° 낮아요"
         location="서울"
       />
+
+      <Button>저장</Button>
+
       <Widget
         type="rainy"
         temperature={-0.7}
         summary="어제보다 0.1° 높아요"
         location="도쿄"
+      />
+
+      <Button disable>취소</Button>
+
+      <Widget
+        type="sunny"
+        temperature={-1.2}
+        summary="어제보다 1.1° 낮아요"
+        location="베이징"
       />
     </div>
   );
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,23 @@
+import './Button.css';
+
+// props: { loading, disable, disabled, mode, type, state, ... }
+
+function Button(props) {
+  let buttonLabel = props.children;
+
+  if (props.loading) {
+    buttonLabel = '⏳ 로딩 중...';
+  }
+
+  return (
+    <button
+      type={props.type ?? 'button'}
+      className="Button"
+      disabled={props.disable}
+    >
+      {buttonLabel}
+    </button>
+  );
+}
+
+export default Button;
