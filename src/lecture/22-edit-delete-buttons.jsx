9682caf0c167a6015cf106e08a1bf013b7e62266
yamoo9 <yamoo9@naver.com>
2024-01-24 10:28:00 +0900

    [실습] 수정, 삭제 버튼 Part 3

--- src/lecture/22-edit-delete-buttons.jsx
+++ src/lecture/22-edit-delete-buttons.jsx
@@ -40,49 +40,44 @@ const IconTrash = ({ size = 16, stroke = 1.5, yPosition = -2 }) => (
   </svg>
 );
 
-const EditButton = ({ children }) => {
+const Button = ({ color, backgroundColor, children }) => (
+  <button
+    type="button"
+    style={{
+      cursor: 'pointer',
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      gap: 4,
+      border: '1px solid',
+      borderColor: color,
+      borderRadius: 4,
+      padding: 8,
+      color,
+      backgroundColor,
+    }}
+  >
+    {children}
+  </button>
+);
+
+const EditButton = ({ color, backgroundColor, children }) => {
   return (
-    <button
-      type="button"
-      style={{
-        cursor: 'pointer',
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        gap: 4,
-        border: '1px solid',
-        borderColor: '#7f7f7f',
-        borderRadius: 4,
-        padding: 8,
-        color: '#7f7f7f',
-        backgroundColor: '#fff',
-      }}
-    >
+    <Button color={color} backgroundColor={backgroundColor}>
       {children}
-    </button>
+    </Button>
   );
 };
 
-const DeleteButton = ({ children }) => {
+const DeleteButton = ({
+  color = '#f53535',
+  backgroundColor = '#fff',
+  children,
+}) => {
   return (
-    <button
-      type="button"
-      style={{
-        cursor: 'pointer',
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        gap: 4,
-        border: '1px solid',
-        borderColor: '#f53535',
-        borderRadius: 4,
-        padding: 8,
-        color: '#f53535',
-        backgroundColor: '#fff',
-      }}
-    >
+    <Button color={color} backgroundColor={backgroundColor}>
       {children}
-    </button>
+    </Button>
   );
 };
 
