9f845a34d69860da671734f7601e743136287972
yamoo9 <yamoo9@naver.com>
2024-01-24 09:36:55 +0900

    [구성] Vite 서버 설정

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-});
+export default defineConfig(
+  /* vite configuration option object */
+  {
+    plugins: [react()],
+    server: {
+      // host: 'localhost', // default: 'localhost'
+      port: 3000, // default: 5173
+      open: true, // default: false
+    },
+  }
+);
