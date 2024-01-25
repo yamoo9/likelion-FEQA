23c60783eba970cdaf87c6b2b6c2b2a23ec8c87c
yamoo9 <yamoo9@naver.com>
2024-01-25 10:25:48 +0900

    [실습] Avatar 컴포넌트 구현

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -1,10 +1,16 @@
 import './Avatar.css';
 
 function Avatar({ photo, name = '', isOnline = false }) {
+  const status = isOnline ? '온라인' : '오프라인';
+
   return (
     <figure className="Avatar">
       <img src={photo} alt={name} />
-      <figcaption>{isOnline ? '온라인' : '오프라인'}</figcaption>
+      <figcaption
+        className={isOnline ? 'online' : ''}
+        aria-label={status}
+        title={status}
+      />
     </figure>
   );
 }
--- src/lecture/26-render-list-avatar-selection.jsx
+++ src/lecture/26-render-list-avatar-selection.jsx
@@ -12,16 +12,20 @@ function Exercise() {
     }
 
     // 랜덤 온/오프라인 설정
-    if (Math.random() >= 0.5) {
-      item.isOnline = true;
-    } else {
-      item.isOnline = false;
-    }
+    // 문
+    // if (Math.random() >= 0.5) {
+    //   item.isOnline = true;
+    // } else {
+    //   item.isOnline = false;
+    // }
+
+    // 식
+    item.isOnline = Math.random() >= 0.5;
 
     return item;
   }); // map 메서드 -> 새로운 배열
+
   // console.log(Object.is(contactData.items, items)); // 복제해서 생성된 배열
-  console.log(items);
 
   // function body 영역
   // 문, 식 모두 사용 가능
