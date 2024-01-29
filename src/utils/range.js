ca5ed80a4aa9d0b679f22788a108529e1b9c1fb9
yamoo9 <yamoo9@naver.com>
2024-01-29 11:48:47 +0900

    [미션] range 함수 작성

---
+++ src/utils/range.js
@@ -0,0 +1,10 @@
+// range 함수 API
+// range(start? = 0, end? = 10, step? = 1) → [0, 1, ..., 10]
+// range(startOrEnd = 20) → [0, 1, ..., 20]
+// range(start = -20, end = 10, step = 10) → [-20, -10, 0, 10]
+
+export default function range(start = 0, end = 10, step = 1) {
+  const output = [];
+
+  return output;
+}
