dccca3b57428a8e3f4e167add6f3e61007290dbf
yamoo9 <yamoo9@naver.com>
2024-01-29 13:05:53 +0900

    [실습] range 유틸리티 함수 part 2

--- src/utils/range.js
+++ src/utils/range.js
@@ -3,8 +3,25 @@
 // range(startOrEnd = 20) → [0, 1, ..., 20]
 // range(start = -20, end = 10, step = 10) → [-20, -10, 0, 10]
 
-export default function range(start = 0, end = 10, step = 1) {
+// range(10, 30) → [10, 11, ..., 30]
+// range(30) → [0, 1, ..., 30]
+// range(10, 30, 10) → [10, 20, 30]
+
+export default function range(startOrEnd, end, step = 1) {
   const output = [];
+
+  if (!end) {
+    end = startOrEnd;
+    startOrEnd = 0;
+  }
+
+  for (
+    let numberValue = startOrEnd;
+    numberValue <= end;
+    numberValue = numberValue + step
+  ) {
+    output.push(numberValue);
+  }
 
   return output;
 }
