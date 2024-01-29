bf5959fec6f25668a8aaf4d8462d0f206aaa5f3d
yamoo9 <yamoo9@naver.com>
2024-01-29 15:04:20 +0900

    [구성] CountUp 컴포넌트

--- src/components/CountUp/CountUp.jsx
+++ src/components/CountUp/CountUp.jsx
@@ -1,5 +1,14 @@
+import classes from './CountUp.module.css';
+
 function CountUp({ count = 0 }) {
-  return <div>CountUp {count}</div>;
+  return (
+    <div className={classes.CountUp}>
+      <output>{count}</output>
+      <button type="button" className={classes.button} aria-label="카운트 업">
+        +1
+      </button>
+    </div>
+  );
 }
 
 export default CountUp;
