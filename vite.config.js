f9876608220c5c3b5fb931a6cbcb53fcc65eab06
yamoo9 <yamoo9@naver.com>
2024-01-31 08:58:59 +0900

    [준비] 9일차 학습 내용

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(
-  /* vite configuration option object */
-  {
-    // base: '/likilion-FEQA/', // '/'
-    plugins: [react()],
-    server: {
-      // host: 'localhost', // default: 'localhost'
-      port: 3000, // default: 5173
-      open: true, // default: false
-    },
-  }
-);
+export default defineConfig({
+  // base: '/likilion-FEQA/',
+  plugins: [react()],
+  server: {
+    port: 3000, // default: 5173
+    open: true, // default: false
+  },
+});
