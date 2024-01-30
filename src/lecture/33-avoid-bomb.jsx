f8b36cb34d053edd849b0d77ef73513601c093c0
yamoo9 <yamoo9@naver.com>
2024-01-30 10:19:58 +0900

    [원론] 왜 리액트는 컴포넌르를 2번씩 렌더링 하나?
    - StrictMode
    - Pure Function Component

--- src/lecture/33-avoid-bomb.jsx
+++ src/lecture/33-avoid-bomb.jsx
@@ -1,8 +1,11 @@
+import { useState } from 'react';
 import classes from './33-avoid-bomb.module.css';
 // console.log(classes);
 
 // jQuery, Vanilla JavaScript
 // 명령형 프로그래밍
+
+let renderCount = 0;
 
 function Exercise() {
   // 리액트 (선언형 프로그래밍)
@@ -11,6 +14,10 @@ function Exercise() {
   // 상태 변경 트리거(요청) → 리액트 감지 → 렌더(가상DOM) → (스냅샷 비교, 재조정)변경이 있을 경우 → 커밋(DOM 반영) → 페인팅
 
   // 어떤 상태??? 게임 재생/일시정지 제어
+  // 게임 중인가요? (isPlaying) => 네(T), 아니오(F) => boolean 타입
+  // 프리미티브(primitive) 타입: 불변(immutable) 데이터 관리
+  const [isPlaying, setIsPlaying] = useState(false /* 초깃값: initial value */);
+
   const gameClassNames = `${classes.game} ${classes.stop}`.trim();
 
   // 이벤트 핸들러
@@ -21,6 +28,19 @@ function Exercise() {
   const handleBomb = () => {
     globalThis.alert('게임 패! 🥲');
   };
+
+  const handleStart = () => {
+    const nextIsPlaying = true;
+    // 상태 변경 요청(trigger) -> 리액트 UI 렌더링(함수 컴포넌트 다시 실행 => JSX 다시 반환)
+    setIsPlaying(nextIsPlaying);
+  };
+
+  const handlePause = () => {
+    const nextIsPlaying = false;
+    setIsPlaying(nextIsPlaying);
+  };
+
+  console.log('render', ++renderCount);
 
   return (
     <>
@@ -39,10 +59,20 @@ function Exercise() {
         </button>
       </div>
       <div className={classes.gameControls} role="group">
-        <button type="button" aria-label="게임 시작" disabled>
+        <button
+          type="button"
+          aria-label="게임 시작"
+          onClick={handleStart}
+          disabled={isPlaying /* true */}
+        >
           start
         </button>
-        <button type="button" aria-label="게임 일시정지">
+        <button
+          type="button"
+          aria-label="게임 일시정지"
+          onClick={handlePause}
+          disabled={!isPlaying /* !false = true */}
+        >
           pause
         </button>
       </div>
--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,15 @@ import App from './app/App';
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
+// StrictMode는 사용 목적 버그의 가능성 검토
+// 리액트 함수 컴포넌트가 순수한가?
+// 순수란? 렌더링 프로세스에 불순(side-effects)한 것이 껴있지 않은가?
+// 순수함의 조건이란?
+//   동일한 것(args)을 전달 받은 함수가 2번 실행되었을 때
+//   동일한 결과(return)를 반환한다면?
+
+// 개발 → 빌드 (StrictMode는 자동 제거) → 배포
+
 root.render(
   <StrictMode>
     <App />
