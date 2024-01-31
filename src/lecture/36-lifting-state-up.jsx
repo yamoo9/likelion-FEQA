eb77c58f9cb44c2395436d90496feb480829f1d8
yamoo9 <yamoo9@naver.com>
2024-01-31 09:39:40 +0900

    [실습] 상태 끌어올리기

--- src/lecture/36-lifting-state-up.jsx
+++ src/lecture/36-lifting-state-up.jsx
@@ -1,3 +1,4 @@
+import classes from './36-lifting-state-up.module.css';
 import { useState } from 'react';
 import { A11yHidden } from '../components';
 
@@ -8,6 +9,24 @@ import { A11yHidden } from '../components';
 // - 다른 형제 또는 저 멀리 있는 컴포넌트와 상태를 공유할 수 없다.
 // - 컴포넌트 사이에 상태를 공유하려면?
 //   공유하려는 컴포넌트 들의 가장 가까운 상위 컴포넌트로 상태를 끌어올려야 한다.
+
+function Accordion() {
+  const headlineId = crypto.randomUUID();
+
+  return (
+    <article className={classes.Accordion} aria-labelledby={headlineId}>
+      <A11yHidden as="h2" id={headlineId}>
+        아코디언을 사용해 컴포넌트 간 상태 공유
+      </A11yHidden>
+      <AccordionPanel>
+        <p>아코디언 컴포넌트는 ..... 1</p>
+      </AccordionPanel>
+      <AccordionPanel>
+        <p>아코디언 컴포넌트는 ..... 2</p>
+      </AccordionPanel>
+    </article>
+  );
+}
 
 function AccordionPanel({ children }) {
   const [isOpen, setIsOpen] = useState(false);
@@ -20,24 +39,6 @@ function AccordionPanel({ children }) {
       </button>
       <div hidden={!isOpen}>{children}</div>
     </div>
-  );
-}
-
-function Accordion() {
-  const headlineId = crypto.randomUUID();
-
-  return (
-    <article className="" aria-labelledby={headlineId}>
-      <A11yHidden as="h2" id={headlineId}>
-        아코디언을 사용해 컴포넌트 간 상태 공유
-      </A11yHidden>
-      <AccordionPanel>
-        <p>아코디언 컴포넌트는 ..... 1</p>
-      </AccordionPanel>
-      <AccordionPanel>
-        <p>아코디언 컴포넌트는 ..... 2</p>
-      </AccordionPanel>
-    </article>
   );
 }
 
