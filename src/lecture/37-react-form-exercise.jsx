1d0f96011018c0b8ff7b4f37e78b1423e2d4c2da
yamoo9 <yamoo9@naver.com>
2024-01-31 13:28:11 +0900

    [실습] 폼 컨트롤 part 4.

--- src/lecture/37-react-form-exercise.jsx
+++ src/lecture/37-react-form-exercise.jsx
@@ -2,19 +2,29 @@
 // jQuery 라이브러리 방식
 // 명령형 프로그래밍
 // 어떻게(HOW)
-// [   ]을 입력하면, 이벤트를 감지해서 [   ]에 출력한다.
+// [    ]을 입력하면, 이벤트를 감지해서 [    ]에 출력한다.
 // vs.
 // 리액트의 방식
 // 어떤 방법 ??? 선언형 프로그래밍
 // 무엇을(WHAT)
 // 제어할 상태를 선언해야 한다.
 
-import { useState } from 'react';
+import { useId, useState } from 'react';
 import { A11yHidden } from '../components';
+
+function Exercise() {
+  return (
+    <div>
+      <h2>폼 컨트롤</h2>
+      <FormExample />
+    </div>
+  );
+}
 
 const INITIAL_FEEL_MESSAGE = '공부하기 좋은 날이네~';
 
-function Exercise() {
+// 컴포넌트 추출
+function FormExample() {
   const [feelMessage, setFeelMessage] = useState(INITIAL_FEEL_MESSAGE);
 
   const handleUpdateFeelMessage = (nextMessage) => {
@@ -26,36 +36,18 @@ function Exercise() {
   };
 
   return (
-    <div>
-      <h2>폼 컨트롤</h2>
+    <>
       <form>
-        <div
-          style={{
-            display: 'flex',
-            gap: 4,
-          }}
-        >
-          {/* 
-            Warning: You provided a `value` prop to a form field without an `onChange` handler. This will render a read-only field. If the field should be mutable use `defaultValue`. Otherwise, set either `onChange` or `readOnly`.
-          */}
-          <label htmlFor="feel-today">오늘 기분</label>
-          <input
-            id="feel-today"
-            type="text"
-            placeholder={INITIAL_FEEL_MESSAGE}
-            // 컨트롤 컴포넌트
-            // value 속성에 리액트가 제어하는 상태가 연결
-            // 리액트가 제어하는 인풋
-            value={feelMessage}
-            // defaultValue={feelMessage}
-            onChange={handleChange}
-            // readOnly={true}
-            // readOnly
-          />
-        </div>
+        <FormInput
+          label="오늘 기분"
+          placeholder={INITIAL_FEEL_MESSAGE}
+          gap={6}
+          value={feelMessage}
+          onChange={handleChange}
+        />
       </form>
 
-      <div style={{ marginBlockStart: 12, display: 'felx', gap: 4 }}>
+      <div style={{ marginBlockStart: 12, display: 'flex', gap: 4 }}>
         <button
           type="button"
           onClick={() => {
@@ -73,6 +65,7 @@ function Exercise() {
           초기화
         </button>
       </div>
+
       <div
         style={{
           border: '1px solid',
@@ -82,9 +75,9 @@ function Exercise() {
           backgroundColor: '#fff',
         }}
       >
-        {/* <output>폼 입력 내용 출력</output> */}
         <output>{feelMessage}</output>
       </div>
+
       <div
         style={{
           border: '1px solid',
@@ -94,22 +87,50 @@ function Exercise() {
           backgroundColor: '#fff',
         }}
       >
-        {/* 
-          Warning: Use the `defaultValue` or `value` props instead of setting children on <textarea>.
-        */}
         <A11yHidden as="label" htmlFor="feel-today-textarea">
           오늘 기분
         </A11yHidden>
-        {/* JSX에서 마크업할 때는 value 또는 defaultValue를 사용해야 해! */}
         <textarea
           id="feel-today-textarea"
           value={feelMessage}
           onChange={handleChange}
         />
-        {/* 리액트에서 JSX를 사용해 마크업할 때 HTML 처럼 안됨!!! */}
-        {/* <textarea>{feelMessage}</textarea> */}
       </div>
-    </div>
+    </>
+  );
+}
+
+function FormInput({
+  as: ComponentName = 'div',
+  type = 'text',
+  label,
+  placeholder,
+  value,
+  onChange,
+  gap = 4,
+  style: customStyle,
+  ...restProps
+}) {
+  const id = useId();
+
+  return (
+    <ComponentName
+      style={{
+        display: 'flex',
+        gap,
+        ...customStyle,
+      }}
+      {...restProps}
+    >
+      <label htmlFor={id}>{label}</label>
+      <input
+        id={id}
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
+    </ComponentName>
   );
 }
 
