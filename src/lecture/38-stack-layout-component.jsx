0138a32f878393e88fcda901ddc265e3a8c3235d
yamoo9 <yamoo9@naver.com>
2024-02-01 11:51:38 +0900

    [실습] 피자 주문 폼 part 7.

--- src/lecture/38-stack-layout-component.jsx
+++ src/lecture/38-stack-layout-component.jsx
@@ -19,10 +19,7 @@ const PIZZA = {
     '밀라노 스폰티니 피자, 시찰리아 칼초네 피자, 시카고 피자, 페페로니 피자, 하와이안 피자'.split(
       ', '
     ),
-  toppings:
-    '새우, 고구마, 감자, 올리브, 페페로니, 치즈, 파인애플, 가지, 불고기'.split(
-      ', '
-    ),
+  toppings: '새우, 고구마, 감자, 올리브, 페페로니'.split(', '),
 };
 
 const INITIAL_ORDER = {
@@ -71,12 +68,15 @@ function Form() {
       nextToppings = orderState.toppings.filter((t) => t !== topping);
     }
 
-    // console.log('다음 토핑 목록: ', nextToppings);
+    const hasFullFilledToppings = nextToppings.length === PIZZA.toppings.length;
 
     const nextOrderState = {
       ...orderState,
       toppings: nextToppings,
+      isAllToppings: hasFullFilledToppings,
     };
+
+    console.log('다음 토핑 목록: ', nextToppings, hasFullFilledToppings);
 
     setOrderState(nextOrderState);
   };
