b163808946e5129ab9a94b21f2d9ca4d7d931a81
yamoo9 <yamoo9@naver.com>
2024-02-01 09:52:02 +0900

    [실습] Stack 컴포넌트 part 2.

--- src/components/Stack/Stack.jsx
+++ src/components/Stack/Stack.jsx
@@ -3,12 +3,23 @@ import classes from './Stack.module.css';
 export default function Stack({
   as: Component = 'div',
   className: customClassNames = '',
+  style: customStyles,
   vertical = false,
   ...restProps
 }) {
-  const classNames = `${classes.Stack} ${customClassNames}`.trim();
+  //
+  const componentClassNames = `${classes.Stack} ${customClassNames}`.trim();
 
-  console.log(vertical);
+  const componentStyles = {
+    '--direction': vertical ? 'column' : 'row',
+    ...customStyles,
+  };
 
-  return <Component className={classNames} {...restProps} />;
+  return (
+    <Component
+      className={componentClassNames}
+      style={componentStyles}
+      {...restProps}
+    />
+  );
 }
