4e319945c670089d51307e207aa788b4c8cc2bfb
yamoo9 <yamoo9@naver.com>
2024-02-02 10:18:16 +0900

    [상황] 컴포넌트 내부에 순수하지 않은 코드에 작성된 경우 3, 4

--- src/lecture/40-side-effects.jsx
+++ src/lecture/40-side-effects.jsx
@@ -1,27 +1,64 @@
 import { Stack } from '@/components';
 
+let renderCount = 0;
+
 function Exercise() {
   // 순수하게 리액트 렌더링에 관한 코드만 사용되어야 한다.
 
-  // 순수하지 않은 코드를 이 곳에 삽입
-  // 웹 문서(DOM)에서 버튼을 찾아서 스타일링 또는 클릭 핸들링을 한다.
-  const buttonElements = document.querySelectorAll('.button');
+  // 순수하지 않은 코드를 이 곳에 삽입 -------------------------------------------------
 
-  buttonElements.forEach((button) => {
+  // [1] 함수 외부의 변수 사용
+  // - 함수 외부의 무언가를 이용해 마크업할 경우 예측이 어렵다.
+  renderCount += 1;
+
+  // [2] DOM 요소 접근, 조작
+  // - 웹 문서(DOM)에서 버튼을 찾아서 스타일링 또는 이벤트 핸들링 하는 것은 리액트 렌더링과 관련이 없다.
+  document.querySelectorAll('.button').forEach((button) => {
     button.style.cssText = `
-      color: #14a256;
-    `;
+        color: #2481af;
+      `;
     button.addEventListener('click', (e) => {
       const color = getComputedStyle(e.target, null).getPropertyValue('color');
       console.log(color);
     });
   });
 
+  // [3] 비동기 처리
+  // - 지연된 시간이 지난 후 어떤 값을 변경한 경우, 리액트 렌더링과 관련이 없다.
+  setTimeout(() => {
+    renderCount += 10;
+    // console.log('1초가 지나 renderCount 값이 +10 되었습니다.');
+    // console.log(`renderCount = ${renderCount}`);
+  }, 1000);
+
+  // [4] 네트워크 요청/응답
+  // - 네트워크를 통해 서버에 요청하여 응답되기 까지 시간이 요구되는데
+  //   리액트의 렌더링 프로세스는 동기적으로 처리되므로 렌더링과 관련이 없다.
+  const productsPromise = fetch(
+    `${
+      import.meta.env.VITE_PB_API
+    }/api/collections/products/records?page=2&perPage=2`
+  );
+
+  console.log(productsPromise);
+
+  // productsPromise
+  // .then((response) => response.json())
+  // .then((data) => renderProducts(data))
+  // .catch((error) => console.error(error));
+
+  // ---------------------------------------------------------------------------
+
   return (
     <Stack vertical className="mx-6">
       <h2 className="text-2xl mt-4">부수 효과(Side Effects)</h2>
-      <Button className="button">순수 함수</Button>
-      <Button className="button">부수 효과</Button>
+      <p>렌더 카운트: {renderCount}</p>
+      <Button className="button" renderCount={renderCount}>
+        순수 함수
+      </Button>
+      <Button className="button" renderCount={renderCount}>
+        부수 효과
+      </Button>
       <ul>
         <li>
           리액트의 컴포넌트는 [ <strong>순수</strong> ] 해야 한다.
@@ -36,10 +73,10 @@ function Exercise() {
   );
 }
 
-function Button({ children, ...restProps }) {
+function Button({ renderCount = 0, children, ...restProps }) {
   return (
     <button type="button" {...restProps}>
-      {children}
+      {children} ({renderCount})
     </button>
   );
 }
