3224f04671c2ef4d8550458d3a927ff9b92c81b9
yamoo9 <yamoo9@naver.com>
2024-02-02 10:30:50 +0900

    [실습] 이벤트 핸들러
    - 사이드 이펙트를 처리하는 공간 1번째

--- src/lecture/40-side-effects.jsx
+++ src/lecture/40-side-effects.jsx
@@ -2,6 +2,10 @@ import { Stack } from '@/components';
 
 let renderCount = 0;
 
+setTimeout(() => {
+  renderCount += 100;
+}, 3000);
+
 function Exercise() {
   // 순수하게 리액트 렌더링에 관한 코드만 사용되어야 한다.
 
@@ -13,15 +17,16 @@ function Exercise() {
 
   // [2] DOM 요소 접근, 조작
   // - 웹 문서(DOM)에서 버튼을 찾아서 스타일링 또는 이벤트 핸들링 하는 것은 리액트 렌더링과 관련이 없다.
-  document.querySelectorAll('.button').forEach((button) => {
-    button.style.cssText = `
-        color: #2481af;
-      `;
-    button.addEventListener('click', (e) => {
-      const color = getComputedStyle(e.target, null).getPropertyValue('color');
-      console.log(color);
-    });
-  });
+  // - gsap, swiper 같이 DOM 기반 애니메이션, 캐러셀 라이브러리를 적용하려는 경우 여기에 해당된다.
+  // document.querySelectorAll('.button').forEach((button) => {
+  //   button.style.cssText = `
+  //       color: #2481af;
+  //     `;
+  //   button.addEventListener('click', (e) => {
+  //     const color = getComputedStyle(e.target, null).getPropertyValue('color');
+  //     console.log(color);
+  //   });
+  // });
 
   // [3] 비동기 처리
   // - 지연된 시간이 지난 후 어떤 값을 변경한 경우, 리액트 렌더링과 관련이 없다.
@@ -49,11 +54,38 @@ function Exercise() {
 
   // ---------------------------------------------------------------------------
 
+  // 그렇다면 리액트에서 사이드 이펙트 코드는 어디에 작성해야 하는가?
+  // 1. 이벤트 핸들러
+  //    - 왜? 이벤트 핸들러 함수 내부에서는 사이드 이펙트 코드 처리가 가능할까?
+  //    - 리액트의 렌더링과 무관하게 실행 시점이 실제 DOM에서 사용자에 의해 실행되기 때문
+  // 2. 이펙트를 처리하기 위한 리액트의 빌트인 훅 : React.useEffect
+  //    특정 시점(라이프 사이클(생명 주기) : 컴포넌트 작동 흐름)에 실행되는 콜백 함수
+
+  // ---------------------------------------------------------------------------
+
   return (
     <Stack vertical className="mx-6">
       <h2 className="text-2xl mt-4">부수 효과(Side Effects)</h2>
       <p>렌더 카운트: {renderCount}</p>
-      <Button className="button" renderCount={renderCount}>
+      <Button
+        className="button"
+        renderCount={renderCount}
+        onClick={() => {
+          console.log(document.querySelectorAll('.button'));
+
+          document.querySelectorAll('.button').forEach((button) => {
+            button.style.cssText = `
+              color: #2481af;
+            `;
+            button.addEventListener('click', (e) => {
+              const color = getComputedStyle(e.target, null).getPropertyValue(
+                'color'
+              );
+              console.log(color);
+            });
+          });
+        }}
+      >
         순수 함수
       </Button>
       <Button className="button" renderCount={renderCount}>
