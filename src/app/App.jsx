f4b53c80a91929f587b94bb00ecfcea6b1611554
yamoo9 <yamoo9@naver.com>
2024-02-02 09:47:33 +0900

    [상황] 컴포넌트 내부에 순수하지 않은 코드에 작성된 경우 1

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,5 +1,5 @@
 import { app as appClasName } from './App.module.css';
-import Exercise from '@/lecture/38-stack-layout-component';
+import Exercise from '@/lecture/40-side-effects';
 
 function App() {
   return (
--- src/lecture/40-side-effects.jsx
+++ src/lecture/40-side-effects.jsx
@@ -1,24 +1,18 @@
 import { Stack } from '@/components';
 
+let renderCount = 0;
+
 function Exercise() {
   // 순수하게 리액트 렌더링에 관한 코드만 사용되어야 한다.
+
+  // 순수하지 않은 코드를 이 곳에 삽입
+  renderCount += 1; // 1
 
   return (
     <Stack vertical className="mx-6">
       <h2 className="text-2xl mt-4">부수 효과(Side Effects)</h2>
-      <button
-        type="button"
-        onClick={(e) => {
-          // console.log('여기서는 사이드 이펙트 코드를 작성할 수 있다.');
-          // DOM 스크립트 사용 가능
-          const listElement = e.target.nextElementSibling;
-          const targetElement = listElement.querySelector('li:nth-child(2)');
-          targetElement.setAttribute('tabindex', -1);
-          targetElement.focus();
-        }}
-      >
-        클릭
-      </button>
+      <Button renderCount={renderCount}>순수 함수</Button>
+      <Button renderCount={renderCount}>부수 효과</Button>
       <ul>
         <li>
           리액트의 컴포넌트는 [ <strong>순수</strong> ] 해야 한다.
@@ -30,6 +24,14 @@ function Exercise() {
         </li>
       </ul>
     </Stack>
+  );
+}
+
+function Button({ renderCount, children, ...restProps }) {
+  return (
+    <button type="button" {...restProps}>
+      {children} ({renderCount})
+    </button>
   );
 }
 
