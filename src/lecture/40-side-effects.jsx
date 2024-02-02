c4894a3172b42defdd22225a221824c3b4b910ca
yamoo9 <yamoo9@naver.com>
2024-02-02 10:42:29 +0900

    [실습] 이벤트 핸들러 part 2
    - 사이드 이펙트를 처리하는 공간 1번째
    - DOM 접근/조작
    - 네트워크 요청/응답

--- src/lecture/40-side-effects.jsx
+++ src/lecture/40-side-effects.jsx
@@ -1,4 +1,5 @@
 import { Stack } from '@/components';
+import { useState } from 'react';
 
 let renderCount = 0;
 
@@ -6,7 +7,13 @@ setTimeout(() => {
   renderCount += 100;
 }, 3000);
 
+const API_ENDPOINT = `${
+  import.meta.env.VITE_PB_API
+}/api/collections/products/records?page=2&perPage=2`;
+
 function Exercise() {
+  const [products, setProducts] = useState(null); // null | Array<{id, name, ...}>
+
   // 순수하게 리액트 렌더링에 관한 코드만 사용되어야 한다.
 
   // 순수하지 않은 코드를 이 곳에 삽입 -------------------------------------------------
@@ -30,22 +37,18 @@ function Exercise() {
 
   // [3] 비동기 처리
   // - 지연된 시간이 지난 후 어떤 값을 변경한 경우, 리액트 렌더링과 관련이 없다.
-  setTimeout(() => {
-    renderCount += 10;
-    // console.log('1초가 지나 renderCount 값이 +10 되었습니다.');
-    // console.log(`renderCount = ${renderCount}`);
-  }, 1000);
+  // setTimeout(() => {
+  // renderCount += 10;
+  // console.log('1초가 지나 renderCount 값이 +10 되었습니다.');
+  // console.log(`renderCount = ${renderCount}`);
+  // }, 1000);
 
   // [4] 네트워크 요청/응답
   // - 네트워크를 통해 서버에 요청하여 응답되기 까지 시간이 요구되는데
   //   리액트의 렌더링 프로세스는 동기적으로 처리되므로 렌더링과 관련이 없다.
-  const productsPromise = fetch(
-    `${
-      import.meta.env.VITE_PB_API
-    }/api/collections/products/records?page=2&perPage=2`
-  );
+  // const productsPromise = fetch(API_ENDPOINT);
 
-  console.log(productsPromise);
+  // console.log(productsPromise);
 
   // productsPromise
   // .then((response) => response.json())
@@ -63,6 +66,29 @@ function Exercise() {
 
   // ---------------------------------------------------------------------------
 
+  const handleEffectDomAccess = () => {
+    console.log(document.querySelectorAll('.button'));
+
+    document.querySelectorAll('.button').forEach((button) => {
+      button.style.cssText = `
+        color: #2481af;
+      `;
+      button.addEventListener('click', (e) => {
+        const color = getComputedStyle(e.target, null).getPropertyValue(
+          'color'
+        );
+        console.log(color);
+      });
+    });
+  };
+
+  const handleEffectNetworkReqRes = () => {
+    fetch(API_ENDPOINT)
+      .then((response) => response.json())
+      .then((data) => setProducts(data.items))
+      .catch((error) => console.error(error));
+  };
+
   return (
     <Stack vertical className="mx-6">
       <h2 className="text-2xl mt-4">부수 효과(Side Effects)</h2>
@@ -70,21 +96,7 @@ function Exercise() {
       <Button
         className="button"
         renderCount={renderCount}
-        onClick={() => {
-          console.log(document.querySelectorAll('.button'));
-
-          document.querySelectorAll('.button').forEach((button) => {
-            button.style.cssText = `
-              color: #2481af;
-            `;
-            button.addEventListener('click', (e) => {
-              const color = getComputedStyle(e.target, null).getPropertyValue(
-                'color'
-              );
-              console.log(color);
-            });
-          });
-        }}
+        onClick={handleEffectDomAccess}
       >
         순수 함수
       </Button>
@@ -101,6 +113,21 @@ function Exercise() {
           사용되어야 한다.
         </li>
       </ul>
+
+      <Button
+        renderCount={products?.length}
+        onClick={handleEffectNetworkReqRes}
+      >
+        상품 요청
+      </Button>
+
+      {products && (
+        <Stack as="ul" vertical gap={12}>
+          {products.map((product) => (
+            <li key={product.id}>{product.title}</li>
+          ))}
+        </Stack>
+      )}
     </Stack>
   );
 }
