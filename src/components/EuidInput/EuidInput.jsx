af86247e53cace44e0b2894a16e58c7123121d01
yamoo9 <yamoo9@naver.com>
2024-02-06 15:52:19 +0900

    [실습] useImperativeHandle 훅

--- src/components/EuidInput/EuidInput.jsx
+++ src/components/EuidInput/EuidInput.jsx
@@ -1,4 +1,4 @@
-import { useId, forwardRef } from 'react';
+import { useId, forwardRef, useImperativeHandle, useRef } from 'react';
 import { A11yHidden } from '@/components';
 
 function EuidInput(
@@ -18,15 +18,41 @@ function EuidInput(
   },
   ref
 ) {
-  console.log(ref);
+  // 컴포넌트 내부에 감춰진(접근 가능하지 않은)
+  // DOM 노드 노출하고 싶지 않다면 고려 (옵션)
+  useImperativeHandle(
+    ref,
+    () => {
+      if (type === 'radio') {
+        console.log('뭔가 수행');
+      }
+
+      return {
+        focus() {
+          inputRef.current.focus();
+        },
+        styling(cssRules) {
+          inputRef.current.style.cssText = cssRules;
+        },
+      };
+    },
+    [type]
+  );
+
+  const inputRef = useRef(null);
+  const labelRef = useRef(null);
 
   const id = useId();
 
-  let labelElement = <label htmlFor={id}>{label}</label>;
+  let labelElement = (
+    <label ref={labelRef} htmlFor={id}>
+      {label}
+    </label>
+  );
 
   if (hiddenLabel) {
     labelElement = (
-      <A11yHidden as="label" htmlFor={id}>
+      <A11yHidden ref={labelRef} as="label" htmlFor={id}>
         {label}
       </A11yHidden>
     );
@@ -43,7 +69,7 @@ function EuidInput(
     >
       {labelElement}
       <input
-        ref={forwardRef}
+        ref={inputRef}
         id={id}
         type={type}
         name={name}
--- src/lecture/44-4-imperative-handle-hook.jsx
+++ src/lecture/44-4-imperative-handle-hook.jsx
@@ -1,5 +1,28 @@
+import { EuidInput } from '@/components';
+import { useEffect, useRef } from 'react';
+
 function Exercise() {
-  return <form></form>;
+  const inputHandlesRef = useRef({});
+
+  useEffect(() => {
+    const { styling } = inputHandlesRef.current;
+
+    styling(`
+      outline: 10px solid blue;
+      padding: 8px;
+    `);
+  }, []);
+
+  return (
+    <form className="flex gap-5 flex-col my-10">
+      <EuidInput
+        ref={inputHandlesRef}
+        label="email"
+        hiddenLabel
+        placeholder="user@company.io"
+      />
+    </form>
+  );
 }
 
 export default Exercise;
