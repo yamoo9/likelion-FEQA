d004512af0c2b34cf477043109da90605c06976e
yamoo9 <yamoo9@naver.com>
2024-02-06 13:28:01 +0900

    [실습] 외부 라이브러리 연결 part 4.

--- src/lecture/42-1-external-lib-dom-control.jsx
+++ src/lecture/42-1-external-lib-dom-control.jsx
@@ -1,22 +1,21 @@
 import { useTilt } from '@/hooks';
+import { range } from '@/utils';
 
-const BOX_COUNT = 9;
 const BOX_OPTIONS = {
   reverse: true,
   glare: true,
   'max-glare': 0.7,
+  scale: 0.94,
 };
 
 function Exercise() {
   return (
     <div className="flex gap-2">
-      {Array(BOX_COUNT)
-        .fill(null)
-        .map((_, i) => (
-          <TiltBox key={i} options={BOX_OPTIONS}>
-            {i + 1}
-          </TiltBox>
-        ))}
+      {range(10, 160, 2).map((n) => (
+        <TiltBox key={n} options={BOX_OPTIONS}>
+          {n}
+        </TiltBox>
+      ))}
     </div>
   );
 }
