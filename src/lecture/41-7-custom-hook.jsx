b92d586878a232c4f7a2c6b859cd11daafcb37ee
yamoo9 <yamoo9@naver.com>
2024-02-06 11:40:27 +0900

    [실습] Intersection Observer API 활용 part 3.

--- src/lecture/41-7-custom-hook.jsx
+++ src/lecture/41-7-custom-hook.jsx
@@ -1,43 +1,42 @@
 import { Stack } from '@/components';
 import { useInOnScreen } from '@/hooks';
 
-function Exercise() {
-  const [isOnScreen, elementRef] = useInOnScreen();
+const colorBoxes = ['black', 'indigo', 'orange', 'emerald'];
 
+function Exercise() {
   return (
     <section className="w-full my-5">
-      <header className="fixed bottom-0 left-0 w-full bg-white/70 text-indigo-900 backdrop-blur-sm p-5">
+      {/* <header className="fixed bottom-0 left-0 w-full bg-white/70 text-indigo-900 backdrop-blur-sm p-5">
         <p>
           INTERSECTION →{' '}
           <b className="font-extrabold">{isOnScreen ? 'IN' : 'OUT'}</b>
         </p>
-      </header>
+      </header> */}
       <h2 className="my-5">
         Intersection Observer{' '}
         <abbr title="Application Programming Interface">API</abbr>
       </h2>
 
       <Stack vertical gap={16}>
-        <Box color="black">Intersection U</Box>
-
-        <Box color="indigo">Intersection P</Box>
-
-        {/* <Box color="emerald">Intersection L</Box> */}
-        <article
-          ref={elementRef}
-          className={`h-[90vh] w-[100%] p-5 bg-emerald-800 text-white rounded-lg`}
-        >
-          <h3 className="uppercase text-2xl">intersection Y</h3>
-        </article>
-
-        <Box color="orange">Intersection Z</Box>
+        {colorBoxes.map((color) => {
+          return (
+            <Box key={color} color={color}>
+              {color.toUpperCase()}
+            </Box>
+          );
+        })}
       </Stack>
     </section>
   );
 }
 
 function Box({ color, children }) {
+  const [inOnScreen, elementRef] = useInOnScreen();
+
+  console.log(`${color} 박스 상태: ${inOnScreen ? 'in' : 'out'}`.toUpperCase());
+
   let bgColor = '';
+
   switch (color) {
     case 'orange':
       bgColor = 'bg-orange-800';
@@ -51,8 +50,10 @@ function Box({ color, children }) {
     default:
       bgColor = 'bg-black';
   }
+
   return (
     <article
+      ref={elementRef}
       className={`h-[90vh] w-[100%] p-5 ${bgColor} text-white rounded-lg`}
     >
       <h3 className="uppercase text-2xl">{children}</h3>
