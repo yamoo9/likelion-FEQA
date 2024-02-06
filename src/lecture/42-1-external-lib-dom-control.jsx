05e55aab8110d71cafca3951fb7c436fa2126631
yamoo9 <yamoo9@naver.com>
2024-02-06 13:38:51 +0900

    [실습] 외부 라이브러리 연결 part 5.
    - 리액트용 라이브러리
    - react-tilt

--- src/lecture/42-1-external-lib-dom-control.jsx
+++ src/lecture/42-1-external-lib-dom-control.jsx
@@ -1,5 +1,6 @@
 import { useTilt } from '@/hooks';
 import { range } from '@/utils';
+import { Tilt } from 'react-tilt';
 
 const BOX_OPTIONS = {
   reverse: true,
@@ -10,13 +11,30 @@ const BOX_OPTIONS = {
 
 function Exercise() {
   return (
-    <div className="flex gap-2">
-      {range(10, 160, 2).map((n) => (
-        <TiltBox key={n} options={BOX_OPTIONS}>
-          {n}
-        </TiltBox>
-      ))}
-    </div>
+    <>
+      <div className="flex gap-2 mb-10">
+        {range(40, 90, 7).map((n) => (
+          <Tilt
+            key={n}
+            className="bg-black"
+            style={{ height: 250, width: 250 }}
+            options={BOX_OPTIONS}
+            onMouseEnter={(e) => {
+              console.log(e);
+            }}
+          >
+            <span>👽 alien</span>
+          </Tilt>
+        ))}
+      </div>
+      <div className="flex gap-2">
+        {range(10, 160, 2).map((n) => (
+          <TiltBox key={n} options={BOX_OPTIONS}>
+            {n}
+          </TiltBox>
+        ))}
+      </div>
+    </>
   );
 }
 
