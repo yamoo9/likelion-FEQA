e09354d7b3eedc04fc86f96c936ca2727f333666
yamoo9 <yamoo9@naver.com>
2024-02-07 14:44:58 +0900

    [원론] 리-렌더링 되어도 값을 기억하려면?
    - useMemo vs. useCallback
    - 객체형 타입 props로 전달
    - 리-렌더링 유발 방지
    - 성능 최적화

--- src/lecture/46-use-callback-vs-use-memo.jsx
+++ src/lecture/46-use-callback-vs-use-memo.jsx
@@ -1,33 +1,92 @@
 import { A11yHidden } from '@/components';
 import { number } from 'prop-types';
-import { useId, useState } from 'react';
+import {
+  useCallback,
+  useEffect,
+  useId,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
+import { useToggle } from '@/hooks';
 
 function Exercise() {
+  const { value: isVisible, toggle } = useToggle(true);
+
   return (
     <div>
-      <h2>useCallback vs. useMemo</h2>
+      {isVisible && <h2>useCallback vs. useMemo</h2>}
+      <button type="button" onClick={toggle}>
+        {isVisible ? '제목 감춤' : '제목 표시'}
+      </button>
       <Counter min={5} count={9} step={2} max={32} />
     </div>
   );
 }
+
+// useCallback 훅
+// - 리-렌더링 할 때 마다 함수 정의가
+//   매번 초기화 되지 않도록 기억(memo, cache)하려고 사용하는 훅
 
 function Counter({ min = 0, count = 0, step = 1, max = 50 }) {
   const id = useId();
 
   const [value, setValue] = useState(count);
 
-  const handleChange = (e) => {
+  // const handleChange = useCallback(함수값, [종속성]);
+  const handleChange = useCallback((e) => {
     const nextValue = Number(e.target.value);
     setValue(nextValue);
-  };
+  }, []);
 
-  const handleCountUp = () => {
+  // ⚠️ value가 바뀌면 다시 기억하기 위해서
+  //   useCallback 다시 함수를 생성해서 캐시한 후 반환
+  // const handleCountUp = useCallback(() => {
+  //   const nextValue = value + step;
+  //   setValue(nextValue);
+  // }, [step, value]);
+
+  // ✅ value가 바뀌어도 다시 기억할 필요가 없다.
+  const handleCountUp = useCallback(() => {
+    // const getNextValue = (v) => v + step;
+    // setValue(getNextValue);
     setValue((v) => v + step);
-  };
+  }, [step]);
 
-  const handleCountDown = () => {
+  // useCallback 예시 (JS 함수 값만 기억)
+  const handleCountDown = useCallback(() => {
     setValue((v) => v - step);
-  };
+  }, [step]);
+
+  // useMemo 예시 (JS 모든 값(함수 포함) 기억)
+  // const memoYourName = useMemo(() => /* 값 기억 */'yamoo' + 9, []);
+  // const handleCountDown2 = useMemo(
+  //   () => () => {
+  //     setValue((v) => v - step);
+  //   },
+  //   [step]
+  // );
+
+  // const countDown = () => {
+  //   setValue((v) => v - step);
+  // };
+
+  const countDownRef = useRef(handleCountDown); // { current: countDown }
+
+  // console.log(countDownRef); // prevCountDown
+
+  useEffect(() => {
+    // console.log('change value');
+    // 이전 렌더링 되었을 때 countDown 함수랑
+    // 다음 렌더링 되었을 때 countDown 함수랑 동일한 참조일까?
+    // prevCountDown vs. countDown
+    const prevCountDown = countDownRef.current;
+
+    console.log(
+      'prevCountDown vs. handleCountDown -> ',
+      Object.is(prevCountDown, handleCountDown)
+    );
+  }, [handleCountDown, value]);
 
   return (
     <div className="flex gap-1 my-5" aria-labelledby={id}>
@@ -39,7 +98,7 @@ function Counter({ min = 0, count = 0, step = 1, max = 50 }) {
         aria-label="카운트 감소"
         title="카운트 감소"
         className="mr-4"
-        onClick={handleCountUp}
+        onClick={handleCountDown}
       >
         -
       </button>
@@ -56,7 +115,7 @@ function Counter({ min = 0, count = 0, step = 1, max = 50 }) {
         type="button"
         aria-label="카운트 증가"
         title="카운트 증가"
-        onClick={handleCountDown}
+        onClick={handleCountUp}
       >
         +
       </button>
