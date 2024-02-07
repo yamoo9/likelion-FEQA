e5740e4ff08ea1d741444cfda97ef36423cd57dc
yamoo9 <yamoo9@naver.com>
2024-02-07 13:18:27 +0900

    [구성] TypeScript용 ESLint 플러그인 & 파서 설치/구성

--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,13 @@ import { createRoot } from 'react-dom/client';
 import App from '@/app/App';
 
 const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
 
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+if (rootElement) {
+  const root = createRoot(rootElement);
+
+  root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+}
