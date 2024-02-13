70315b6796a9d765ca95cdfc6a0e2257cc5654a7
yamoo9 <yamoo9@naver.com>
2024-02-13 16:51:02 +0900

    [원론] React Router 클라이언트 측 라우팅 (CSR) part 5.

--- src/pages/layout/GlobalNavBar.jsx
+++ src/pages/layout/GlobalNavBar.jsx
@@ -1,20 +1,46 @@
 import { A11yHidden } from '@/components';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import S from './GlobalNavBar.module.css';
+import { memo } from 'react';
+
+const navigationItems = [
+  {
+    id: 'home',
+    path: '/',
+    text: '홈',
+  },
+  {
+    id: 'intro',
+    path: '/intro',
+    text: '소개',
+  },
+  {
+    id: 'products',
+    path: '/products',
+    text: '상품',
+  },
+];
+
+const assignActiveClassNames = ({ isActive }) => {
+  const activeClassName = isActive ? S.active : '';
+  return `${S.link} ${activeClassName}`.trim();
+};
 
 function GlobalNavBar() {
   return (
     <nav className=" bg-white text-slate-800 p-5 w-full shadow-md">
       <A11yHidden as="h2">글로벌 내비게이션 바</A11yHidden>
-      <ul className="text-xs flex space-x-4">
-        <li>
-          <Link to="/">홈</Link>
-        </li>
-        <li>
-          <Link to="/intro">소개</Link>
-        </li>
+      <ul className="text-xs flex">
+        {navigationItems.map((item) => (
+          <li key={item.id}>
+            <NavLink to={item.path} className={assignActiveClassNames}>
+              {item.text}
+            </NavLink>
+          </li>
+        ))}
       </ul>
     </nav>
   );
 }
 
-export default GlobalNavBar;
+export default memo(GlobalNavBar);
