0897ff64a179412443f028ba865db5ff014d2a7b
yamoo9 <yamoo9@naver.com>
2024-02-15 07:18:58 +0900

    [수정사항 09] 이미지 최적화 (Image Optimization)

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,32 @@ import path from 'node:path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+import viteImagemin from '@vheemstra/vite-plugin-imagemin';
+import imageminGifSicle from 'imagemin-gifsicle';
+import imageminMozjpeg from 'imagemin-mozjpeg';
+import imageminPngQuant from 'imagemin-pngquant';
+import imageminSvgo from 'imagemin-svgo';
+import imageminWebp from 'imagemin-webp';
+
 export default defineConfig({
   base: '/EUID',
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteImagemin({
+      plugins: {
+        jpg: imageminMozjpeg(),
+        png: imageminPngQuant(),
+        gif: imageminGifSicle(),
+        svg: imageminSvgo(),
+      },
+      makeWebp: {
+        plugins: {
+          jpg: imageminWebp(),
+          png: imageminWebp(),
+        },
+      },
+    }),
+  ],
   server: {
     port: 3000, // default: 5173
     open: true, // default: false
