ec63c73af45a0bcb8f1d8ea4d43a6d8b8cc65e90
yamoo9 <yamoo9@naver.com>
2024-02-15 02:51:50 +0900

    [수정사항 02] 글로벌 내비게이션 바
    - 스타일링 수정
    - 활성 링크 초점 이동 설정
    - 스크롤바 감춤 (기능 유지)

--- src/pages/layouts/GlobalNavBar.jsx
+++ src/pages/layouts/GlobalNavBar.jsx
@@ -1,30 +1,40 @@
 import { memo } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import S from './GlobalNavBar.module.css';
 import { A11yHidden } from '@/components';
 import navigationItems from '@/routes/navigation';
 
 const assignActiveClassNames = ({ isActive }) => {
   const activeClassName = isActive ? S.active : '';
-  return `${S.link} ${activeClassName}`.trim();
+  return `${S.link} ${activeClassName} text-nowrap -outline-offset-2`.trim();
 };
 
 function GlobalNavBar() {
+  const { pathname } = useLocation();
+
   return (
-    <nav className=" bg-white text-slate-800 w-full shadow-md">
+    <nav className="bg-white text-slate-800 w-full shadow-md">
       <A11yHidden as="h2">학습 주제</A11yHidden>
-      <ul className="text-xs flex">
+      <ul className="text-xs flex overflow-x-auto snap-x">
         {navigationItems
           .filter((route) => {
             return !route.path || !route.path.includes(':');
           })
-          .map((item) => (
-            <li key={item.id}>
-              <NavLink to={item.path} className={assignActiveClassNames}>
-                {item.text}
-              </NavLink>
-            </li>
-          ))}
+          .map((item) => {
+            const isActive = pathname === item.path;
+
+            return (
+              <li key={item.id} className="snap-center">
+                <NavLink
+                  to={item.path}
+                  tabIndex={isActive ? -1 : null}
+                  className={assignActiveClassNames}
+                >
+                  {item.text}
+                </NavLink>
+              </li>
+            );
+          })}
       </ul>
     </nav>
   );
