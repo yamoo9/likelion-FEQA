c5e09b5ea71ebac86b3412ec8d3721838908feea
yamoo9 <yamoo9@naver.com>
2024-02-15 02:45:46 +0900

    [수정사항 01] 라우트 베이스 네임 - Vite 베이스 구성 맞춤

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,8 +1,4 @@
-import {
-  Route,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 
 // 레이아웃(Layouts)
 import RootLayout from '@/pages/layouts/RootLayout';
@@ -12,11 +8,6 @@ import NotFound from '@/pages/NotFound';
 
 // 내비게이션(Navigation)
 import navigationItems from './navigation';
-import HomePage from '@/pages/Home';
-import IntroPage from '@/pages/IntroContext';
-import NonBlockUIPage from '@/pages/NonBlockUI';
-import AdminLayout from '@/pages/layouts/AdminLayout';
-import { ProtectRoute } from '@/components';
 
 // 루트 구성(routes configuration)
 const routes = [
@@ -29,35 +20,11 @@ const routes = [
 ];
 
 const options = {
-  // basename: '/euid'
+  basename: import.meta.env.BASE_URL,
 };
 
 // 라우터 인스턴스 생성
 let router = createBrowserRouter(routes, options);
-
-// JSX 라우터 구성 + 라우터 인스턴스 생성
-router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<RootLayout />} errorElement={<NotFound />}>
-      <Route path="" element={<HomePage />} />
-      <Route path="intro" element={<IntroPage />} />
-      <Route
-        path="non-block-ui-with-updating-state"
-        element={<NonBlockUIPage />}
-      />
-      <Route
-        path="/admin"
-        element={
-          <ProtectRoute isAllowed={true} redirectPath="/signin">
-            <AdminLayout />
-          </ProtectRoute>
-        }
-      >
-        {/* .... */}
-      </Route>
-    </Route>
-  )
-);
 
 // 라우터 인스턴스 기본 내보내기
 export default router;
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  // base: '/likilion-FEQA/',
+  base: '/euid',
   plugins: [react()],
   server: {
     port: 3000, // default: 5173
