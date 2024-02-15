eb15f7d4567bbdfd6e5966e396fc482960e51056
yamoo9 <yamoo9@naver.com>
2024-02-15 14:54:02 +0900

    [원론] useReducer 훅 활용 part 2.

--- src/pages/LearnReducer.jsx
+++ src/pages/LearnReducer.jsx
@@ -1,12 +1,41 @@
 import { getDocumentTitle } from '@/utils';
 import { Helmet } from 'react-helmet-async';
 import { FormInput } from '@/components';
-import { useRef, useState } from 'react';
+import { useRef, useReducer } from 'react';
+
+// 리듀서 함수
+const manageMessages = (state, action /* { type, payload? } */) => {
+  // ADD ACTION
+  if (action.type === '메시지/추가') {
+    // 새로운 상태가 반환
+    return {
+      ...state,
+      messages: [action.payload, ...state.messages],
+    };
+  }
+
+  // EDIT ACTION
+
+  // DELETE ACTION
+
+  // DEFAULT ACTION
+  return state;
+};
+
+// 초깃값(관리할 상태)
+const INIT_MESSAGES_INFO = {
+  messages: ['wow'],
+};
 
 export function Component() {
   const inputRef = useRef(null);
-  const [messages, setMessages] = useState(['wow']);
-  const [newMessage, setNewMessage] = useState('');
+  // const [messages, setMessages] = useState(['wow']);
+  // const [newMessage, setNewMessage] = useState('');
+
+  const [messageState, dispatch] = useReducer(
+    manageMessages,
+    INIT_MESSAGES_INFO
+  );
 
   return (
     <>
@@ -21,29 +50,32 @@ export function Component() {
         onSubmit={(e) => {
           e.preventDefault();
 
-          setMessages((m) => [newMessage, ...m]);
-          setNewMessage('');
+          const formData = new FormData(e.target);
+          const newMessage = formData.get('message');
 
-          inputRef.current.focus();
+          // setMessages((m) => [newMessage, ...m]);
+          // setNewMessage('');
+
+          const addAction = {
+            type: '메시지/추가',
+            payload: newMessage,
+          };
+
+          dispatch(addAction);
+
+          const input = inputRef.current;
+          input.value = '';
+          input.focus();
         }}
       >
-        <FormInput
-          ref={inputRef}
-          name="text"
-          label="message"
-          hiddenLabel
-          value={newMessage}
-          onChange={(e) => {
-            setNewMessage(e.target.value);
-          }}
-        >
+        <FormInput ref={inputRef} name="message" label="message" hiddenLabel>
           메시지
         </FormInput>
         <button type="submit">추가</button>
       </form>
 
       <ul className="my-5">
-        {messages.map((m, i) => (
+        {messageState.messages.map((m, i) => (
           <li key={i}>{m}</li>
         ))}
       </ul>
