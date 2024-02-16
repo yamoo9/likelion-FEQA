ded221e109113b755bdfbddf3c353ef5e467758a
yamoo9 <yamoo9@naver.com>
2024-02-16 10:15:50 +0900

    [원론] Zustand 라이브러리 장점
    - 페이지 간, 글로벌 상태 공유 가능
    - 비교적 쉽다 (React, Redux)
    - 내부적으로 셀렉터 함수를 사용하므로 성능 최적화에도 좋다.

--- src/pages/components/counter/Counter.jsx
+++ src/pages/components/counter/Counter.jsx
@@ -1,60 +1,38 @@
 import { A11yHidden } from '@/components';
-import { Component, Suspense, lazy, useCallback, useState } from 'react';
 import useCountStore from '@/store/useCountStore';
+import { Suspense, lazy } from 'react';
 
 const CountButton = lazy(() => import('./CountButton'));
 const CountDisplay = lazy(() => import('./CountDisplay'));
 
-export class CompareWithFunctionComponent extends Component {
-  state = {
-    a: 1,
-    b: 1,
-  };
-
-  render() {
-    return (
-      <button type="button" onClick={this.update.bind(this)}>
-        update state
-      </button>
-    );
-  }
-
-  update() {
-    this.setState({
-      b: this.state.b + 2,
-    });
-  }
-}
-
 function Counter() {
-  useCountStore();
+  // useCountStore(/* selector : memory data */);
 
-  // const [state, setState] = useState({
-  //   a: 1,
-  //   b: 1,
-  // });
+  // 상태 값만 선택자 함수를 통해 추출하는 경우
+  // const count = useCountStore((state/* { count } */) => state.count);
 
-  // const handler = () => {
-  //   setState({
-  //     //...state, // { a, b }
-  //     b: state.b + 2,
-  //   });
-  // };
+  // 상태, 상태 업데이트 함수(plus, minus, set, reset) 중 일부 추출하는 경우
+  const [count, plus, minus] = useCountStore(({ count, plus, minus }) => [
+    count,
+    plus,
+    minus,
+  ]);
 
-  const [count, setCount] = useState(0);
+  // const handleDecrementCount = useCallback(() => setCount((c) => c - 1), []);
+  // const handleDecrementCount = () => minus();
 
-  const handleDecrementCount = useCallback(() => setCount((c) => c - 1), []);
-  const handleIncrementCount = useCallback(() => setCount((c) => c + 1), []);
+  // const handleIncrementCount = useCallback(() => setCount((c) => c + 1), []);
+  // const handleIncrementCount = () => plus();
 
   return (
     <div className="inline-flex items-center gap-2 bg-white px-3 py-1 rounded-full shadow">
       <A11yHidden>카운터 예제</A11yHidden>
       <Suspense fallback={<span>로딩 중...</span>}>
-        <CountButton aria-label="카운터 감소" onClick={handleDecrementCount}>
+        <CountButton aria-label="카운터 감소" onClick={minus}>
           -
         </CountButton>
         <CountDisplay>{count}</CountDisplay>
-        <CountButton aria-label="카운터 증가" onClick={handleIncrementCount}>
+        <CountButton aria-label="카운터 증가" onClick={plus}>
           +
         </CountButton>
       </Suspense>
